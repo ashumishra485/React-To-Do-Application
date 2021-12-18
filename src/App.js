9b88ccb911b25fea5e89dc92a1025901f2d898ba
Ashutosh Mishra <55360225+ashumishra485@users.noreply.github.com>
2021-12-18 21:47:28 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ListItem from './ListItem';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {faTrash} from '@fortawesome/free-solid-svg-icons';
 
+
 library.add(faTrash);
 class App extends React.Component {
   constructor(props){
